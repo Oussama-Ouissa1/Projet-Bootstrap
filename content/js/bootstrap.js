df27628520f2ecbff7ff55292833b383848ae2c0
Oussama-Ouissa1 <oussamaaouissa2006@gmail.com>
2024-10-26 17:59:50 +0100

    Projet BOOTSTRAP

--- content/js/bootstrap.js
+++ content/js/bootstrap.js
@@ -4500,16 +4500,16 @@ const cardContainer = document.getElementById('card-container');
 // Fonction pour créer une carte HTML à partir des données
 function createCard(data) {
     return `
-        <div class="card mb-3" style="max-width: 540px; margin: 20px">
+        <div class="card mb-3 " style="margin: 20px; max-width: 900px">
             <div class="row g-0">
                 <div class="col-md-4">
-                    <img src="${data.image}" class="img-fluid rounded-start" alt="${data.title}">
+                    <img src="${data.image}" class="img-fluid rounded" alt="${data.title}">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title">${data.title}</h5>
                         <p class="card-text">${data.description}</p>
-                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                        <p class="card-text d-flex justify-content-end"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
             </div>
